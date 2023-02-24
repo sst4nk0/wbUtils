1274417ed8792c9355a2ba9b590cba4d7cce46b0
Anton Menov <anmeno@ttu.ee>
2023-02-24 11:04:13 +0200

    Refactor variables so that they match Code Conventions

--- src/main/java/wb/plugin/wbutils/events/JoinQuitEvent.java
+++ src/main/java/wb/plugin/wbutils/events/JoinQuitEvent.java
@@ -11,35 +11,45 @@ import java.util.Random;
 
 
 public class JoinQuitEvent implements Listener {
-    Random rand = new Random();
+    private final Random random = new Random();
 
+    /**
+     * Do something.
+     *
+     * @param jEvent parameter
+     */
     @EventHandler
-    public void onPlayerJoin(@NotNull PlayerJoinEvent jevent){
-        if(jevent.getPlayer().hasPlayedBefore()){
-            switch (rand.nextInt(0, 7)) {
-                case 0 -> jevent.setJoinMessage("加 " + ChatColor.YELLOW + jevent.getPlayer().getDisplayName() + " почтил нас своим присутствием.");
-                case 1 -> jevent.setJoinMessage("加 " + ChatColor.YELLOW + jevent.getPlayer().getDisplayName() + " решил заглянуть к нам.");
-                case 2 -> jevent.setJoinMessage("加 " + ChatColor.YELLOW + jevent.getPlayer().getDisplayName() + " в деле, всем молчать.");
-                case 3 -> jevent.setJoinMessage("加 " + ChatColor.YELLOW + jevent.getPlayer().getDisplayName() + " среди нас.");
-                case 4 -> jevent.setJoinMessage("加 " + ChatColor.YELLOW + jevent.getPlayer().getDisplayName() + " врывается в игру!");
-                case 5 -> jevent.setJoinMessage("加 " + ChatColor.YELLOW + jevent.getPlayer().getDisplayName() + " незаметно проник к нам...");
-                case 6 -> jevent.setJoinMessage("加 " + ChatColor.YELLOW + jevent.getPlayer().getDisplayName() + " хочет пообщаться!");
+    public void onPlayerJoin(final @NotNull PlayerJoinEvent jEvent) {
+        if (jEvent.getPlayer().hasPlayedBefore()) {
+            switch (random.nextInt(0, 7)) {
+                case 0 -> jEvent.setJoinMessage("加 " + ChatColor.YELLOW + jEvent.getPlayer().getDisplayName() + " почтил нас своим присутствием.");
+                case 1 -> jEvent.setJoinMessage("加 " + ChatColor.YELLOW + jEvent.getPlayer().getDisplayName() + " решил заглянуть к нам.");
+                case 2 -> jEvent.setJoinMessage("加 " + ChatColor.YELLOW + jEvent.getPlayer().getDisplayName() + " в деле, всем молчать.");
+                case 3 -> jEvent.setJoinMessage("加 " + ChatColor.YELLOW + jEvent.getPlayer().getDisplayName() + " среди нас.");
+                case 4 -> jEvent.setJoinMessage("加 " + ChatColor.YELLOW + jEvent.getPlayer().getDisplayName() + " врывается в игру!");
+                case 5 -> jEvent.setJoinMessage("加 " + ChatColor.YELLOW + jEvent.getPlayer().getDisplayName() + " незаметно проник к нам...");
+                case 6 -> jEvent.setJoinMessage("加 " + ChatColor.YELLOW + jEvent.getPlayer().getDisplayName() + " хочет пообщаться!");
             }
-        }
-        else{
-            jevent.setJoinMessage("加 " + ChatColor.YELLOW + jevent.getPlayer().getDisplayName() + " впервые тут. Приветствуем!");
+        } else {
+            jEvent.setJoinMessage("加 " + ChatColor.YELLOW + jEvent.getPlayer().getDisplayName() + " впервые тут. Приветствуем!");
         }
     }
+
+    /**
+     * Do something.
+     *
+     * @param qEvent parameter
+     */
     @EventHandler
-    public void onPlayerLeave(@NotNull PlayerQuitEvent qevent){
-        switch (rand.nextInt(0, 7)) {
-            case 0 -> qevent.setQuitMessage("放 " + ChatColor.YELLOW + qevent.getPlayer().getDisplayName() + " устал от нашей компании.");
-            case 1 -> qevent.setQuitMessage("放 " + ChatColor.YELLOW + qevent.getPlayer().getDisplayName() + " решил сделать перерыв.");
-            case 2 -> qevent.setQuitMessage("放 " + ChatColor.YELLOW + qevent.getPlayer().getDisplayName() + " засиделся.");
-            case 3 -> qevent.setQuitMessage("放 " + ChatColor.YELLOW + qevent.getPlayer().getDisplayName() + " обрадовал нас своим уходом!");
-            case 4 -> qevent.setQuitMessage("放 " + ChatColor.YELLOW + qevent.getPlayer().getDisplayName() + " ушёл по-английски...");
-            case 5 -> qevent.setQuitMessage("放 " + ChatColor.YELLOW + qevent.getPlayer().getDisplayName() + " отправился в реальность.");
-            case 6 -> qevent.setQuitMessage("放 " + ChatColor.YELLOW + qevent.getPlayer().getDisplayName() + " нашёл кнопку отключения...");
+    public void onPlayerLeave(final @NotNull PlayerQuitEvent qEvent) {
+        switch (random.nextInt(0, 7)) {
+            case 0 -> qEvent.setQuitMessage("放 " + ChatColor.YELLOW + qEvent.getPlayer().getDisplayName() + " устал от нашей компании.");
+            case 1 -> qEvent.setQuitMessage("放 " + ChatColor.YELLOW + qEvent.getPlayer().getDisplayName() + " решил сделать перерыв.");
+            case 2 -> qEvent.setQuitMessage("放 " + ChatColor.YELLOW + qEvent.getPlayer().getDisplayName() + " засиделся.");
+            case 3 -> qEvent.setQuitMessage("放 " + ChatColor.YELLOW + qEvent.getPlayer().getDisplayName() + " обрадовал нас своим уходом!");
+            case 4 -> qEvent.setQuitMessage("放 " + ChatColor.YELLOW + qEvent.getPlayer().getDisplayName() + " ушёл по-английски...");
+            case 5 -> qEvent.setQuitMessage("放 " + ChatColor.YELLOW + qEvent.getPlayer().getDisplayName() + " отправился в реальность.");
+            case 6 -> qEvent.setQuitMessage("放 " + ChatColor.YELLOW + qEvent.getPlayer().getDisplayName() + " нашёл кнопку отключения...");
         }
     }
 }
