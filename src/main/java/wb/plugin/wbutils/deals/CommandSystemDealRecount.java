2369dc2eb1d380fcb16e6b19e97470497bbbf616
Anton Menov <anmeno@ttu.ee>
2023-02-24 11:29:26 +0200

    Make code pass more Style Checks

--- src/main/java/wb/plugin/wbutils/deals/CommandSystemDealRecount.java
+++ src/main/java/wb/plugin/wbutils/deals/CommandSystemDealRecount.java
@@ -6,44 +6,57 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 public class CommandSystemDealRecount implements CommandExecutor {
+
+    /**
+     * Do something.
+     * @param sender  sender
+     * @param command command
+     * @param label   label
+     * @param args    args
+     * @return some boolean
+     */
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (sender instanceof Player) return true;
-        if (!sender.hasPermission("wbutils.dealrecount")) return true;
-        if (args.length<1){
+    public boolean onCommand(final @NotNull CommandSender sender, final @NotNull Command command, final @NotNull String label, final @NotNull String[] args) {
+        if (sender instanceof Player) { return true; }
+        if (!sender.hasPermission("wbutils.dealrecount")) { return true; }
+        if (args.length < 1) {
             System.out.println("[CONSOLE] [msg] [Ошибка ввода. Пример: /dealrecount <deal_id>]");
             return true;
         }
 
-        int coins_copper = Integer.parseInt(FileDealsData.get().getString(args[0]+".coins_copper"));
-        int coins_silver = Integer.parseInt(FileDealsData.get().getString(args[0]+".coins_silver"));
-        int coins_gold = Integer.parseInt(FileDealsData.get().getString(args[0]+".coins_gold"));
+        int coinsCopper = Integer.parseInt(Objects.requireNonNull(FileDealsData.get().getString(args[0] + ".coinsCopper")));
+        int coinsSilver = Integer.parseInt(Objects.requireNonNull(FileDealsData.get().getString(args[0] + ".coinsSilver")));
+        int coinsGold = Integer.parseInt(Objects.requireNonNull(FileDealsData.get().getString(args[0] + ".coinsGold")));
         boolean changed = false;
 
+        final int maxStackSize = 64;
 
-        if (coins_copper >=64){
-            while (coins_copper >= 64){
-                coins_copper-=64;
-                coins_silver++;
+        if (coinsCopper >= maxStackSize) {
+            while (coinsCopper >= maxStackSize) {
+                coinsCopper -= maxStackSize;
+                coinsSilver++;
             }
             changed = true;
         }
 
-        if (coins_silver >= 64){
-            while (coins_silver >= 64){
-                coins_silver -= 64;
-                coins_gold++;
+        if (coinsSilver >= maxStackSize) {
+            while (coinsSilver >= maxStackSize) {
+                coinsSilver -= maxStackSize;
+                coinsGold++;
             }
             changed = true;
         }
 
-        if (changed){
-            FileDealsData.get().set(args[0]+".coins_copper", coins_copper);
-            FileDealsData.get().set(args[0]+".coins_silver", coins_silver);
-            FileDealsData.get().set(args[0]+".coins_gold", coins_gold);
+        if (changed) {
+            FileDealsData.get().set(args[0] + ".coinsCopper", coinsCopper);
+            FileDealsData.get().set(args[0] + ".coinsSilver", coinsSilver);
+            FileDealsData.get().set(args[0] + ".coinsGold", coinsGold);
         }
 
         return true;
     }
+
 }
