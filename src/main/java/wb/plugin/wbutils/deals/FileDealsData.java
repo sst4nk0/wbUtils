a19a4381521a1a1383d8e86705500fd1fd7ece76
Anton Menov <anmeno@ttu.ee>
2023-02-24 11:34:05 +0200

    Make code pass more Style Checks

--- src/main/java/wb/plugin/wbutils/deals/FileDealsData.java
+++ src/main/java/wb/plugin/wbutils/deals/FileDealsData.java
@@ -6,36 +6,45 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 public class FileDealsData {
+
     private static File file;
     private static FileConfiguration dataFile;
 
-    public static void setup(){
-        file = new File(Bukkit.getServer().getPluginManager().getPlugin("wbUtils").getDataFolder(),"dealsdata.yml");
+    /**
+     * Do something.
+     */
+    public static void setup() {
+        file = new File(Objects.requireNonNull(Bukkit.getServer().getPluginManager().getPlugin("wbUtils")).getDataFolder(), "dealsdata.yml");
 
-        if (!file.exists()){
-            try{
+        if (!file.exists()) {
+            try {
                 file.createNewFile();
-            }catch (IOException e){
-
-            }
+            } catch (IOException ignored) { }
         }
         dataFile = YamlConfiguration.loadConfiguration(file);
     }
 
-
-    public static FileConfiguration get(){
+    /**
+     * Do something.
+     * @return something
+     */
+    public static FileConfiguration get() {
         return dataFile;
     }
-    public static void save(){
-        try{
+
+    public static void save() {
+        try {
             dataFile.save(file);
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Couldn't save deals data file");
         }
     }
-    public static void reload(){
+
+    public static void reload() {
         dataFile = YamlConfiguration.loadConfiguration(file);
     }
+
 }
