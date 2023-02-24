36c128943990c484d90693f16a37e83566c6e48e
Anton Menov <anmeno@ttu.ee>
2023-02-24 11:44:02 +0200

    Make code pass more Style Checks

--- src/main/java/wb/plugin/wbutils/deals/TabCompleterDealInfo.java
+++ src/main/java/wb/plugin/wbutils/deals/TabCompleterDealInfo.java
@@ -10,40 +10,50 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TabCompleterDealInfo implements TabCompleter {
+
+    /**
+     * Do something.
+     * @param sender  sender
+     * @param command command
+     * @param alias   alias
+     * @param args    args
+     * @return some List
+     */
     @Override
-    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+    public @Nullable List<String> onTabComplete(final @NotNull CommandSender sender, final @NotNull Command command, final @NotNull String alias, final @NotNull String[] args) {
         List<String> commands = new ArrayList<>();
-        switch (args.length){
-            case 1 ->{
+        switch (args.length) {
+            case 1 -> {
                 List<String> tabs = new ArrayList<>();
                 tabs.add("1");
                 tabs.add("2");
                 tabs.add("3");
-                for (String s : tabs){
-                    if (s.toLowerCase().startsWith(args[0].toLowerCase())){
+                for (String s : tabs) {
+                    if (s.toLowerCase().startsWith(args[0].toLowerCase())) {
                         commands.add(s);
                     }
                 }
                 return commands;
             }
-            case 2 ->{
+            case 2 -> {
                 List<String> tabs = new ArrayList<>();
                 tabs.add("owner");
                 tabs.add("materials");
                 tabs.add("coins_gold");
                 tabs.add("coins_silver");
                 tabs.add("coins_copper");
-                for (String s : tabs){
-                    if (s.toLowerCase().startsWith(args[1].toLowerCase())){
+                for (String s : tabs) {
+                    if (s.toLowerCase().startsWith(args[1].toLowerCase())) {
                         commands.add(s);
                     }
                 }
                 return commands;
             }
-            case 3 ->{
+            case 3 -> {
                 return commands;
             }
         }
         return null;
     }
+
 }
