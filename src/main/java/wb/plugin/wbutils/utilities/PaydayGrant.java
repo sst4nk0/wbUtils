ad4bd916fcc8aa44712e70af0e494b4b82927b55
sst4nk0 <salbenfl@gmail.com>
2023-04-14 22:49:37 +0300

    Make code pass more Style Checks

--- src/main/java/wb/plugin/wbutils/utilities/PaydayGrant.java
+++ src/main/java/wb/plugin/wbutils/utilities/PaydayGrant.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 
 public class PaydayGrant {
 
-    private static final HashMap<String, String> notificationsList = new HashMap<>();; // player(send to) + message(what to send)
+    private static final HashMap<String, String> warningsToSend = new HashMap<>();; // player(send to) + message(what to send)
     private static final HashMap<Integer, String> dealsToReset = new HashMap<>(); // dealId + Owner
 
 
@@ -31,20 +31,23 @@ public class PaydayGrant {
      */
     public void resetDealOwners() {
 
+        // Обнуляем сделки
         dealsToReset.forEach((key, value) -> {
             DatabaseDeals.setOwner(key, "-");
             DatabaseDeals.setMaterials(key, "16");
-            notificationsList.put(value, ChatColor.LIGHT_PURPLE + "我 Ваша сделка была разорвана.");
-        } );  // Обнуляем сделки
+            warningsToSend.put(value, ChatColor.LIGHT_PURPLE + "我 Ваша сделка была разорвана.");
+        } );
 
-        dealsToReset.clear(); // Очистка очереди на обнуление
+        // Очистка очереди на обнуление
+        dealsToReset.clear();
 
+        // Пополнение очереди на обнуление
         for (int i = 1; i <= DatabaseDeals.getDealsQuantity(); i++) {
             if (Integer.parseInt(DatabaseDeals.getMaterials(i)) <= -8) {
                 dealsToReset.put(i, DatabaseDeals.getOwner(i));
-                notificationsList.put(DatabaseDeals.getOwner(i), ChatColor.LIGHT_PURPLE + "我 Ваша сделка вот-вот будет разорвана.");
+                warningsToSend.put(DatabaseDeals.getOwner(i), ChatColor.LIGHT_PURPLE + "我 Ваша сделка вот-вот будет разорвана.");
             }
-        } // Пополнение очереди на обнуление
+        }
     }
 
 
@@ -52,21 +55,24 @@ public class PaydayGrant {
      * Broadcasts special payday notification to everyone online.
      */
     public void sendNotification() {
-        String SPACE_STRING = null;
+        String SPACE_STRING = "";
+
         for (Player playersOnline : Bukkit.getOnlinePlayers()) {
             playersOnline.sendMessage(SPACE_STRING);
             new TimeSyncReallife(playersOnline);
             playersOnline.sendMessage(ChatColor.YELLOW + "   Спасибо за игру на нашем сервере!");
             playersOnline.sendMessage(SPACE_STRING);
 
-            notificationsList.forEach((key, value) -> {
+            // Отправлем всем спец сообщения из очереди
+            warningsToSend.forEach((key, value) -> {
                Player player = Bukkit.getPlayerExact(key);
                player.sendMessage(value);
                player.playSound(player.getLocation(), "custom.pagania.warning-1", 1, 1);
                player.sendMessage(SPACE_STRING);
-            } );  // Отправлем всем сообщения из очереди
+            } );
 
-            notificationsList.clear(); // Очистка очереди уведомлений
+            // Очистка очереди уведомлений
+            warningsToSend.clear();
         }
     }
 }
