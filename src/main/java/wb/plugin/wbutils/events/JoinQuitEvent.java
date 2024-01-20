209e7182814d727f023a32af037f651829ca6e8c
Anton Menov <antonmenov@gmail.com>
2024-01-20 23:38:15 +0200

    Use not deprecated PlayerEvent.joinMessage

--- src/main/java/wb/plugin/wbutils/events/JoinQuitEvent.java
+++ src/main/java/wb/plugin/wbutils/events/JoinQuitEvent.java
@@ -1,55 +1,68 @@
 package wb.plugin.wbutils.events;
 
-import org.bukkit.ChatColor;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
 import java.util.Random;
 
 
 public class JoinQuitEvent implements Listener {
+
     private final Random random = new Random();
 
-    /**
-     * Do something.
-     *
-     * @param jEvent parameter
-     */
     @EventHandler
     public void onPlayerJoin(final @NotNull PlayerJoinEvent jEvent) {
-        if (jEvent.getPlayer().hasPlayedBefore()) {
+        final TextComponent prefix = Component.text("加 ");
+        final TextComponent content;
+        final Player player = jEvent.getPlayer();
+        final String playerName = player.getDisplayName();
+        final NamedTextColor color = NamedTextColor.YELLOW;
+        if (player.hasPlayedBefore()) {
             switch (random.nextInt(0, 7)) {
-                case 0 -> jEvent.setJoinMessage("加 " + ChatColor.YELLOW + jEvent.getPlayer().getDisplayName() + " почтил нас своим присутствием.");
-                case 1 -> jEvent.setJoinMessage("加 " + ChatColor.YELLOW + jEvent.getPlayer().getDisplayName() + " решил заглянуть к нам.");
-                case 2 -> jEvent.setJoinMessage("加 " + ChatColor.YELLOW + jEvent.getPlayer().getDisplayName() + " в деле, всем молчать.");
-                case 3 -> jEvent.setJoinMessage("加 " + ChatColor.YELLOW + jEvent.getPlayer().getDisplayName() + " среди нас.");
-                case 4 -> jEvent.setJoinMessage("加 " + ChatColor.YELLOW + jEvent.getPlayer().getDisplayName() + " врывается в игру!");
-                case 5 -> jEvent.setJoinMessage("加 " + ChatColor.YELLOW + jEvent.getPlayer().getDisplayName() + " незаметно проник к нам...");
-                case 6 -> jEvent.setJoinMessage("加 " + ChatColor.YELLOW + jEvent.getPlayer().getDisplayName() + " хочет пообщаться!");
+                case 0 -> content = Component.text(playerName + " почтил нас своим присутствием.", color);
+                case 1 -> content = Component.text(playerName + " решил заглянуть к нам.", color);
+                case 2 -> content = Component.text(playerName + " в деле, всем молчать.", color);
+                case 3 -> content = Component.text(playerName + " среди нас.", color);
+                case 4 -> content = Component.text(playerName + " врывается в игру!", color);
+                case 5 -> content = Component.text(playerName + " незаметно проник к нам...", color);
+                case 6 -> content = Component.text(playerName + " хочет пообщаться!", color);
+                default -> throw new IllegalStateException("Unexpected value.");
             }
         } else {
-            jEvent.setJoinMessage("加 " + ChatColor.YELLOW + jEvent.getPlayer().getDisplayName() + " впервые тут. Приветствуем!");
+            content = Component.text(playerName + " впервые тут. Приветствуем!", color);
         }
+
+        final TextComponent message = prefix.append(Objects.requireNonNull(content));
+        jEvent.joinMessage(message);
     }
 
-    /**
-     * Do something.
-     *
-     * @param qEvent parameter
-     */
     @EventHandler
     public void onPlayerLeave(final @NotNull PlayerQuitEvent qEvent) {
+        final TextComponent prefix = Component.text("放 ");
+        final TextComponent content;
+        final Player player = qEvent.getPlayer();
+        final String playerName = player.getDisplayName();
+        final NamedTextColor color = NamedTextColor.YELLOW;
         switch (random.nextInt(0, 7)) {
-            case 0 -> qEvent.setQuitMessage("放 " + ChatColor.YELLOW + qEvent.getPlayer().getDisplayName() + " устал от нашей компании.");
-            case 1 -> qEvent.setQuitMessage("放 " + ChatColor.YELLOW + qEvent.getPlayer().getDisplayName() + " решил сделать перерыв.");
-            case 2 -> qEvent.setQuitMessage("放 " + ChatColor.YELLOW + qEvent.getPlayer().getDisplayName() + " засиделся.");
-            case 3 -> qEvent.setQuitMessage("放 " + ChatColor.YELLOW + qEvent.getPlayer().getDisplayName() + " обрадовал нас своим уходом!");
-            case 4 -> qEvent.setQuitMessage("放 " + ChatColor.YELLOW + qEvent.getPlayer().getDisplayName() + " ушёл по-английски...");
-            case 5 -> qEvent.setQuitMessage("放 " + ChatColor.YELLOW + qEvent.getPlayer().getDisplayName() + " отправился в реальность.");
-            case 6 -> qEvent.setQuitMessage("放 " + ChatColor.YELLOW + qEvent.getPlayer().getDisplayName() + " нашёл кнопку отключения...");
+            case 0 -> content = Component.text(playerName + " устал от нашей компании.", color);
+            case 1 -> content = Component.text(playerName + " решил сделать перерыв.", color);
+            case 2 -> content = Component.text(playerName + " засиделся.", color);
+            case 3 -> content = Component.text(playerName + " обрадовал нас своим уходом!", color);
+            case 4 -> content = Component.text(playerName + " ушёл по-английски...", color);
+            case 5 -> content = Component.text(playerName + " отправился в реальность.", color);
+            case 6 -> content = Component.text(playerName + " нашёл кнопку отключения...", color);
+            default -> throw new IllegalStateException("Unexpected value.");
         }
+
+        final TextComponent message = prefix.append(Objects.requireNonNull(content));
+        qEvent.quitMessage(message);
     }
 }
