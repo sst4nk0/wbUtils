d284a4cf8fb6230777003f502baaea40d04b51c3
Anton Menov <antonmenov@gmail.com>
2024-01-24 17:30:42 +0200

    Refactor DealInfoPlaceholder

--- src/main/java/wb/plugin/wbutils/adapters/placeholders/DealInfoPlaceholder.java
+++ src/main/java/wb/plugin/wbutils/adapters/placeholders/DealInfoPlaceholder.java
@@ -1,9 +1,9 @@
 package wb.plugin.wbutils.adapters.placeholders;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.OfflinePlayer;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import wb.plugin.wbutils.adapters.repositories.DealsRepository;
 import wb.plugin.wbutils.entities.Deal;
 
@@ -11,103 +11,57 @@ public class DealInfoPlaceholder extends PlaceholderExpansion {
 
     private final DealsRepository databaseDeals;
 
-    /**
-     * Initialize a constructor.
-     */
     public DealInfoPlaceholder(final DealsRepository databaseDeals) {
         this.databaseDeals = databaseDeals;
     }
 
-    /**
-     * Do something.
-     *
-     * @return some boolean.
-     */
     @Override
     public boolean persist() {
         return true;
     }
 
-    /**
-     * Do something.
-     *
-     * @return some string
-     */
     @Override
     public @NotNull String getIdentifier() {
         return "wbutils";
     }
 
-    /**
-     * Do something.
-     *
-     * @return some string
-     */
     @Override
     public @NotNull String getAuthor() {
         return "sst4nk0";
     }
 
-    /**
-     * Do something.
-     *
-     * @return some string
-     */
     @Override
     public @NotNull String getVersion() {
         return "0.2.0";
     }
 
-    /**
-     * Do something.
-     *
-     * @return some boolean
-     */
     @Override
     public boolean canRegister() {
         return true;
     }
 
-    /**
-     * Do something.
-     *
-     * @param player     player
-     * @param identifier identifier
-     * @return some String
-     */
     @Override
-    public String onPlaceholderRequest(final Player player, final String identifier) {
-        if (identifier.startsWith("dealowner_")) {
-            int dealId = Integer.parseInt(identifier.replace("dealowner_", ""));
-            Deal deal = databaseDeals.getDeal(dealId);
-            return deal.owner();
-        } else if (identifier.startsWith("dealcopper_")) {
-            int dealId = Integer.parseInt(identifier.replace("dealcopper_", ""));
-            Deal deal = databaseDeals.getDeal(dealId);
-            return deal.coins_copper();
-        } else if (identifier.startsWith("dealsilver_")) {
-            int dealId = Integer.parseInt(identifier.replace("dealsilver_", ""));
-            Deal deal = databaseDeals.getDeal(dealId);
-            return deal.coins_silver();
-        } else if (identifier.startsWith("dealgold_")) {
-            int dealId = Integer.parseInt(identifier.replace("dealgold_", ""));
-            Deal deal = databaseDeals.getDeal(dealId);
-            return deal.coins_gold();
-        } else if (identifier.startsWith("dealmaterials_")) {
-            int dealId = Integer.parseInt(identifier.replace("dealmaterials_", ""));
-            Deal deal = databaseDeals.getDeal(dealId);
-            return deal.materials();
-        } else if (identifier.startsWith("dealstatus_")) {
-            int dealId = Integer.parseInt(identifier.replace("dealstatus_", ""));
-            Deal deal = databaseDeals.getDeal(dealId);
-            int dealMaterials = Integer.parseInt(deal.materials());
-            if (dealMaterials > -1) {
-                return " ";
-            } else {
-                return "(сделка неустойчива)";
+    public @Nullable String onRequest(final OfflinePlayer player, final @NotNull String params) {
+        System.out.println("Identifier: " + params);
+        if (params.startsWith("deal")) {
+            final String[] identifierSplit = params.split("_");
+            final String dealProperty = identifierSplit[0];
+            final int dealId = Integer.parseInt(identifierSplit[1]);
+            final Deal deal = databaseDeals.getDeal(dealId);
+            if (deal == null) {
+                return null;
             }
-        } else {
-            return null;
+
+            return switch (dealProperty) {
+                case "dealowner" -> deal.owner();
+                case "dealcopper" -> deal.coins_copper();
+                case "dealsilver" -> deal.coins_silver();
+                case "dealgold" -> deal.coins_gold();
+                case "dealmaterials" -> deal.materials();
+                case "dealstatus" -> Integer.parseInt(deal.materials()) > -1 ? " " : "(сделка неустойчива)";
+                default -> throw new IllegalStateException("Unexpected params: " + params);
+            };
         }
+        return null;
     }
 }
